cc1659c42cda5783d246fc68ffe2c839876745bb
Asaduzzaman <ajaman375@gmail.com>
2021-10-17 12:46:38 -0700

    header correction

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Common/Header/Header';
+import Home from './components/Home/Main/Home';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
     <Header/>
     <Switch>
       <Route exact path="/">
-        
+        <Home></Home>
       </Route>
     </Switch>
   </Router>
---
+++ src/components/Home/Banner/Banner.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { GiftFilled ,HeartFilled , ThunderboltFilled,CarryOutFilled } from '@ant-design/icons';
+
+const Banner = () => {
+    return (
+        <header className="header">
+            <img  src="https://i.imgur.com/1sEY9gH.jpg" alt="" srcset="" />
+            <div className="header-content care-bg rounded shadow">
+                <h1 className="fw-bolder we-text">WE CARE</h1>
+                <h2 className="fw-bolder about-text">ABOUT YOU <span className="text-white">AND</span></h2>
+                <h1 className="fw-bolder smile-text text-white shadow">YOUR SIMLE</h1>
+            </div>
+            <div className="container  header-content-two">
+                <div className="row">
+                    <div className="col-md-4 bg-danger p-3 text-center text-white">
+                        <h3>Experience Best Dental</h3>
+                    </div>
+                    <div className="col-md-6 d-flex justify-content-between best-bg">
+                        <div className="fs-1 mx-5"><HeartFilled /> </div>
+                        <div className="fs-1 mx-3 "><CarryOutFilled /></div>
+                        <div className="fs-1 mx-3"><GiftFilled /></div>
+                        <div className="fs-1 me-5"><ThunderboltFilled /></div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Banner;
